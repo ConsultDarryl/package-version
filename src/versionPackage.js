706596f6d8935b130a292fc447a8e0345fc83291
Darryl Brown <darrylbrown@darrylbrown.co.uk>
2022-05-07 19:38:36 +0100

    Increment build number

--- src/versionPackage.js
+++ src/versionPackage.js
@@ -1,38 +1,28 @@
 'use strict';
 
 const fs = require('fs');
-const crypto = require('crypto');
 const execSync = require('child_process').execSync;
-const autonum = require("bent")("https://autonum.app", "POST", "json", 200);
 
 const gitFormat = '{%n  "commit": "%H",%n  "short_commit": "%h",%n  "refs": "%D",%n  "author": "%aN"%n}';
 
 async function versionPackage(pkgFile, flags) {
   const commit = {};
-  
+
   if (flags.data) {
     commit.data = flags.data;
   }
 
   if (flags['add-commit']) {
     const output = execSync(`git log -n 1 '--pretty=format:${gitFormat}'`).toString();
-    try { Object.assign(commit, JSON.parse(output)); } catch(e) { }
+    try { Object.assign(commit, JSON.parse(output)); } catch (e) { }
   }
 
   const pkgContent = JSON.parse(fs.readFileSync(pkgFile).toString());
-  
-  if (!flags.secret) {
-    const md5sum = crypto.createHash('md5');
-    md5sum.update(`${pkgContent.name}${pkgContent.author}${pkgContent.author}`);
-    flags.secret = md5sum.digest('base64');
-  }
 
-  const pkgKey = `${pkgContent.name}-${flags.secret}`.replace(/[^a-z0-9]+/gi, '-');
+  const build = Number.parseInt(pkgContent.version.split('.').pop());
+  const version = pkgContent.version.replace(/\d+$/, build + 1);
 
-  const { number } = await autonum(`/v1/next?id=${pkgKey}`, commit);
-  const version = pkgContent.version.replace(/\d+$/, number);
-  
-  console.log(`${pkgKey} updated to version ${version}`);
+  console.log(`version updated to ${version}`);
 
   pkgContent.version = version;
   fs.writeFileSync(pkgFile, JSON.stringify(pkgContent, null, 2));
